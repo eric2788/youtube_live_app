2365c81b12bb96fef7694fca4131066daf42bf0d
eric2788 <tsukiko852@gmail.com>
2021-09-19 22:18:34 +0800

    optimze code

--- src/SpiderClient.ts
+++ src/SpiderClient.ts
@@ -2,7 +2,7 @@ import { setIntervalAsync, SetIntervalAsyncTimer } from 'set-interval-async/dyna
 import { clearIntervalAsync } from 'set-interval-async'
 import { checker } from '../config/config.json'
 import { getChannelName, getLiveStreamVideo, getLiveStatus } from './YoutubeApi'
-import { BraodCastInfo, LiveBroadcast, LiveRoomStatus, LIVE_ROOM_STATUS_CHANNEL, StandAloneRedisClient } from './types'
+import { BraodCastInfo, LiveBroadcast, LiveRoomStatus, LiveStatus, LIVE_ROOM_STATUS_CHANNEL, StandAloneRedisClient } from './types'
 
 const INTERVAL = checker.interval // seconds
 
@@ -13,8 +13,7 @@ export class SpiderClient {
 
     private _timer: SetIntervalAsyncTimer | null = null
 
-    private _broadcasted: boolean = false
-    private _upcoming: boolean = false
+    private _lastStatus: LiveStatus = 'idle'
 
     constructor(channel: string, client: StandAloneRedisClient) {
         this._channel = channel
@@ -26,8 +25,7 @@ export class SpiderClient {
         const status = await getLiveStatus(this._channel)
          if (status != 'idle') {
             console.log(`頻道 ${this.channel} ${status == 'live' ? '正在直播' : '有預定直播'}`)
-            if (status == 'live' && this._broadcasted) return // save quota
-            if (status == 'upcoming' && this._upcoming) return // save quota
+            if (this._lastStatus == status) return // save quota
             const video = await getLiveStreamVideo(this._channel, status == 'upcoming')
             let info: BraodCastInfo | undefined = undefined
             let name: string
@@ -51,12 +49,7 @@ export class SpiderClient {
                 status: status,
                 info: info
             })
-            if (status == 'live'){
-                this._broadcasted = true
-            }
-            if (status == 'upcoming') {
-                this,this._upcoming = true
-            }
+            this._lastStatus = status
         } else {
             console.debug(`頻道 ${this.channel} 並沒有在直播`)
             await this.publish({
@@ -64,8 +57,8 @@ export class SpiderClient {
                 channelName: await getChannelName(this.channel),
                 status: status
             })
-            if (this._broadcasted){
-                this._broadcasted = false
+            if (this._lastStatus != 'idle'){
+                this._lastStatus = 'idle'
             }
         }
     }
